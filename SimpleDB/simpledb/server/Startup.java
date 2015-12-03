468ee45af399792af4557588e650e1b7fb4bc2a9
Sudhansu Singh <sudhansusingh22@live.in>
2015-12-02 22:16:16 -0500

    modified code to handle port already in use exception

--- SimpleDB/simpledb/server/Startup.java
+++ SimpleDB/simpledb/server/Startup.java
@@ -1,16 +1,22 @@
 package simpledb.server;
 
 import simpledb.remote.*;
+
+import java.rmi.RemoteException;
 import java.rmi.registry.*;
 
 public class Startup {
    public static void main(String args[]) throws Exception {
       // configure and initialize the database
       SimpleDB.init(args[0]);
-      
+      Registry reg;
       // create a registry specific for the server on the default port
-      Registry reg = LocateRegistry.createRegistry(1099);
-      
+      try{
+    	   reg = LocateRegistry.createRegistry(1099);
+      }
+      catch(RemoteException e){
+    	  reg = LocateRegistry.getRegistry(1099);
+      }
       // and post the server entry in it
       RemoteDriver d = new RemoteDriverImpl();
       reg.rebind("simpledb", d);
